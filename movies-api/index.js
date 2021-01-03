0ba3f0e429be1cf87ab933aab967693c7f4291b3
Kate Furlong <furlong.kd@gmail.com>
2021-01-03 18:51:05 +0000

    Week 9 Error Handling

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,9 +1,18 @@
 import dotenv from "dotenv";
 import express from "express";
-import bodyParser from 'body-parser';
+import bodyParser from "body-parser";
 import moviesRouter from "./api/movies";
 
 dotenv.config();
+
+const errHandler = (err, req, res, next) => {
+  /* if the error is in development then send the stack trace to display the whole error,
+  if it's in production then just send the error message  */
+  if (process.env.NODE_ENV === "production") {
+    return res.status(500).send(`Something went wrong!`);
+  }
+  res.status(500).send(`Hey!! You caught the error 👍👍, ${err.stack} `);
+};
 
 const app = express();
 
@@ -15,6 +24,8 @@ app.use(bodyParser.urlencoded());
 
 app.use(express.static("public"));
 app.use("/api/movies", moviesRouter);
+
+app.use(errHandler);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
