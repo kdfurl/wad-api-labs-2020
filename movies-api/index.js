4fddc0e83fa07920ba7407d140cd5d06755f661e
Kate Furlong <furlong.kd@gmail.com>
2021-01-03 17:55:18 +0000

    added routing for add, update, delete movie

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
+import bodyParser from 'body-parser';
 import moviesRouter from "./api/movies";
 
 dotenv.config();
@@ -7,6 +8,10 @@ dotenv.config();
 const app = express();
 
 const port = process.env.PORT;
+
+//configure body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
 app.use(express.static("public"));
 app.use("/api/movies", moviesRouter);
